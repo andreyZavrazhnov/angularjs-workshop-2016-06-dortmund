9048433da92cc034f175c2e4a29da324dbae26b2
Sascha Brink <sascha.brink@gmail.com>
2016-06-03 13:43:42 +0200

    Add book filter

--- app/book-index.component.js
+++ app/book-index.component.js
@@ -3,6 +3,22 @@
 angular.module('myApp')
   .component('bookIndex', {
     templateUrl: 'app/book-index.component.html',
-    controller: function() {
+    controller: function($http) {
+      var $ctrl = this;
+
+      $http.get('http://bookmonkey-api.angularjs.de/books')
+        .then(function(response) {
+          $ctrl.books = response.data;
+        });
+
+      $ctrl.searchTermFn = function(searchTerm) {
+        return function(book) {
+          if(!searchTerm) {
+            return true;
+          }
+          return book.title.indexOf(searchTerm) !== -1
+            ||  book.author.indexOf(searchTerm) !== -1;
+        }
+      }
     }
   });
