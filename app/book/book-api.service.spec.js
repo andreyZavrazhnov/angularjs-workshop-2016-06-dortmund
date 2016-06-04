9c7ccc95ccea15d25cc2ff96d3eaa94ccf3fe859
Sascha Brink <sascha.brink@gmail.com>
2016-06-04 12:19:34 +0200

    Refactor bookApi test

--- app/book/book-api.service.spec.js
+++ app/book/book-api.service.spec.js
@@ -1,21 +1,25 @@
 describe('A book api service', function() {
+  var bookApi;
 
   beforeEach(module('myApp'));
+  beforeEach(inject(function(_bookApi_) {
+    bookApi = _bookApi_;
+  }));
 
   describe('with #bookSearchFilter', function() {
 
-    it('should be defined', inject(function(bookApi) {
+    it('should be defined', function() {
       expect(bookApi.bookSearchFilter).toBeDefined();
-    }));
+    });
 
-    it('should filter a book successfully', inject(function(bookApi) {
+    it('should filter a book successfully', function() {
       var book = {
         title: 'ngularJAYASS',
         author: 'Quentin'
       };
 
       expect(bookApi.bookSearchFilter('Q')(book)).toBeTruthy();
-    }));
+    });
 
   });
 
